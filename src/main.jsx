610d8b6ee9c82e51d169cf3eae51528010564912
akash <ghosakash94@gmail.com>
2026-07-11 22:21:35 +0530

    Fixed

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 
 import store from "./store/store";
 
-import "./index.css";
+import "./assets/custom.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
